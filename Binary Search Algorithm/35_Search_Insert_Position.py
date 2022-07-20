9aa8fe114624d871fa43cc07a4a59dd3ae24e2d7
yufu6 <56329012+yufu6@users.noreply.github.com>
2022-07-20 15:34:06 +0800

    Update 35_Search_Insert_Position.py

--- Binary Search Algorithm/35_Search_Insert_Position.py
+++ Binary Search Algorithm/35_Search_Insert_Position.py
@@ -7,3 +7,37 @@ class Solution:
                 return i
             i = i + 1
         return len(nums)
+
+ 
+# Using Binary Search Algorithm
+# left close right open
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)
+        while left < right:
+            mid = left + ((right - left) // 2)
+            
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid
+            else:
+                return mid
+        
+        return right
+    
+#left lose right close
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + ((right - left) // 2)
+            
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                return mid
+        
+        return right + 1
