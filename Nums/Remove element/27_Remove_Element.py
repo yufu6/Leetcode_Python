81cb6682b6c573b9aea5ab7e2ad7325377cefd11
yufu6 <56329012+yufu6@users.noreply.github.com>
2022-07-21 17:14:36 +0800

    Update 27_Remove_Element.py

--- Nums/Remove element/27_Remove_Element.py
+++ Nums/Remove element/27_Remove_Element.py
@@ -13,3 +13,30 @@ class Solution:
         
         return slow
                 
+#Solution 2 改变了元素相对位置，确保了移动最少元素
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int leftIndex = 0;
+        int rightIndex = nums.size() - 1;
+        while (leftIndex <= rightIndex) {
+            // 找左边等于val的元素
+            while (leftIndex <= rightIndex && nums[leftIndex] != val){
+                cout << "left" << leftIndex;
+                ++leftIndex;
+            }
+            // 找右边不等于val的元素
+            while (leftIndex <= rightIndex && nums[rightIndex] == val) {
+                cout << "right" << rightIndex;
+                -- rightIndex;
+            }
+            // 将右边不等于val的元素覆盖左边等于val的元素
+            if (leftIndex < rightIndex) {
+                
+                cout << "rn" << nums[rightIndex];
+                nums[leftIndex++] = nums[rightIndex--];
+            }
+        }
+        return leftIndex;   // leftIndex一定指向了最终数组末尾的下一个元素
+    }
+};
